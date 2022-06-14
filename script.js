3da9e99b78e2607d6eecb2be75e4c075b24c42c7
aaronlrv <aaronvitharanage@gmail.com>
2022-06-14 11:21:20 +0400

    Add basic calculation functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add (a,b){
+    let total = a+b
+    return total
+}
+
+function subtract (a,b){
+    let total = a-b
+    return total
+}
+
+function multiply (a,b){
+    let total = a*b
+    return total
+}
+
+function divide (a,b){
+    let total = a / b
+    return total
+}
+
+console.log(add(2,7))
+console.log(subtract(7,2))
+console.log(multiply(9,9))
+console.log(divide(4,2))
