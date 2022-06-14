d322e477a3b5f7be72293c3a557e74e168c96763
aaronlrv <aaronvitharanage@gmail.com>
2022-06-14 11:25:47 +0400

    Add basic operator function too choose between calculation functions

--- script.js
+++ script.js
@@ -18,7 +18,20 @@ function divide (a,b){
     return total
 }
 
-console.log(add(2,7))
-console.log(subtract(7,2))
-console.log(multiply(9,9))
-console.log(divide(4,2))
+
+function operate(a,b,operator){
+    if (operator === +){
+        function add (a,b)
+    } else if (operator === -){
+        function subtract (a,b)
+    } else if (operator === *){
+        function multiply (a,b)
+    } else {
+        function divide (a,b)
+    }
+}
+
+console.log(add(2,7)) /// 9
+console.log(subtract(7,2)) /// 5
+console.log(multiply(9,9)) /// 82
+console.log(divide(4,2)) /// 2
